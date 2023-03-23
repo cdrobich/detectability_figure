a7002207e3e169b3cf7e3910f68cd74e94718210
Courtney Robichaud <44408770+cdrobich@users.noreply.github.com>
2023-03-23 11:31:43 -0400

    Updating phylopic

--- scripts/figure_practice.R
+++ scripts/figure_practice.R
@@ -65,7 +65,7 @@ ggplot() +
   xlab("") +
   ylab(" ") +
   theme_minimal() +
-  theme(text = element_text(size = 16)) +
+  theme(text = element_text(size = 26)) +
   ggtitle("CANADA") +
   scale_y_continuous(breaks = seq(0, 1, 0.2), limits = c(0, 1)) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
@@ -83,7 +83,7 @@ usa_pic <- usa %>%
   xlab("") +
   ylab(" ") +
   theme_minimal() +
-  theme(text = element_text(size = 16),
+  theme(text = element_text(size = 26),
         axis.text.y=element_blank(),axis.ticks=element_blank(),
         plot.title = element_text(hjust = 0.5)) +
   ggtitle("USA") +
@@ -95,12 +95,7 @@ usa_pic <- usa %>%
 
 # Put it together
 
-combine <- usa_pic + plot_spacer() + can_pic +
-  plot_layout(widths = c(5, -1.05, 5),
-              guides = 'collect')
-
-combine
-
+  
 
 ggsave("figures/combined_figure.TIFF", combine,
        width = 15.8,
@@ -111,6 +106,7 @@ ggsave("figures/combined_figure.TIFF", combine,
 library(ggalt)
 
 data_combined <- read.csv("data/data_combined.csv")
+colnames(data_combined)
 
 combined_wide <- data_combined %>% 
   pivot_wider(names_from = Country,
@@ -174,175 +170,108 @@ canada_raw %>%
 # reptile - loggerhead sea turtle, eastern box turtle, snapping turtle
 
 #### MAMMAL ####
-bear <- name_search(text = "Grizzly bear", options = "namebankID")[[1]] 
-bear
 
-bear_id_all <- name_images(uuid = bear$uid[1])  # list images
-bear_id_all
-
-
-bear_id <- name_images(uuid = bear$uid[1])$same[[1]]$uid  # get individual image id
-bear_id
-
-bear_pic <- image_data(bear_id, size = 256)[[1]]
+bear_uuid <- get_uuid(name = "Grizzly bear", n = 1)
+bear_img <- get_phylopic(uuid = bear_uuid)
 
 #### AMPHIBIAN ####
 
-toad <- name_search(text = "Bufo", options = "namebankID")[[1]]
-toad
-
-toad_id_all <- name_images(uuid = toad$uid[1])  # list images
-toad_id_all
-
-toad_pic <- image_data("e1a404c9-4edc-45c3-a694-0817157832fc", size = 256)[[1]]
+toad <- get_uuid(name = "Bufo", n = 1)
+toad_img <- get_phylopic(uuid = toad)
 
 ##### Fish ####
 
-fish <- name_search(text = "Salmoninae", options = "namebankID")[[1]]
-fish
-
-fish_id <- name_images(uuid = fish$uid[1])
-
-fish_pic <- image_data("68304786-f291-4115-9ccd-ead068ba6f19", size = 256)[[1]]
-
+fish <- get_uuid(name = "Salvelinus", n = 1)
+fish_img <- get_phylopic(uuid = fish)
 
 #### Birds #####
 
-bird <- name_search(text = "Charadriidae", options = "namebankID")[[1]]
-bird
-
-bird_id <- name_images(uuid = bird$uid[1])
-
-bird_pic <- image_data("5ccc0a37-77ae-4e69-b824-ad3e85f802c6", size = 256)[[1]]
+bird <- get_uuid(name = "Charadriidae", n = 1)
+bird_img <- get_phylopic(uuid = bird)
 
 ###### Reptiles ####
 
-reptile <- name_search(text = "snapping turtle", options = "namebankID")[[1]]
-reptile
+reptile <- get_uuid(name = "Crotalus viridis", n = 1)
+reptile_img <- get_phylopic(uuid = reptile)
 
-reptile_id <- name_images(uuid = reptile$uid[3])
-
-reptile_pic <- image_data("fec4ae89-b3ea-4693-b63e-85dad8bc9ed3", size = 256)[[1]]
 
 ##### Flower ####
 
-flower <- name_search(text = "Helianthus annuus ", options = "namebankID")[[1]]
-flower
-
-flower_id <- name_images(uuid = flower$uid[1])
-
-flower_pic <- image_data("40b91c6c-4ef8-4a5a-9b90-85fb50d74c5b", size = 256)[[1]]
+flower <- get_uuid(name = "Helianthus annuus", n = 1)
+flower_img <- get_phylopic(uuid = flower)
 
 
 ##### Insects ####
 
-insect <- name_search(text = "monarch", options = "namebankID")[[1]]
-insect
-
-insect_id <- name_images(uuid = insect$uid[1])
-
-insect_pic <- image_data("e6705593-91ba-4b6e-9dfd-515005e35fc6", size = 256)[[1]]
-
+insect <- get_uuid(name = "monarch",n = 1)
+insect_img <- get_phylopic(uuid = insect)
 
 
 ###### Clams ####
-clam <- name_search(text = "clam", options = "namebankID")[[1]]
-clam
+clam <- get_uuid(name = "Neonucula pratasensis", n = 1)
+clam_img <- get_phylopic(uuid = clam)
 
-clam_id <- name_images(uuid = clam$uid[23])
-
-clam_pic <- image_data("70f895b9-ac43-4d4f-bd8c-604834bce948", size = 256)[[1]]
 
 ####### LICHEN ####
 
-lichen <- name_search(text = "Lecanoromycetes", options = "namebankID")[[1]]
-lichen
+lichen <- get_uuid(name = "Lecanoromycetes", n = 1)
+lichen_img <- get_phylopic(uuid = lichen)
 
-lichen_id <- name_images(uuid = lichen$uid[1])
-
-lichen_pic <- image_data( "c918dfe0-a7da-48c1-bc82-78d7041e4a13", size = 256)[[1]]
 
 ##### SNAILS #########
-snail<- name_search(text = "gastropoda", options = "namebankID")[[1]]
-snail
-
-snail_id <- name_images(uuid = snail$uid[1])
-
-snail_pic <- image_data("c5835123-e2d3-4c20-9e7a-f7b6528bbf8e", size = 256)[[1]]
+snail<- get_uuid(name = "Myosotella myosotis", n = 1)
+snail_img <- get_phylopic(uuid = snail)
 
 ########## Ferns and Allies ##########
 
-fern<- name_search(text = "Monilophyta", options = "namebankID")[[1]]
-fern
-
-fern_id <- name_images(uuid = fern$uid[1])
-
-fern_pic <- image_data("f2d82e69-89b2-4164-9dfe-e9e623cbd848", size = 256)[[1]]
-
+fern <- get_uuid(name = "Monilophyta", n = 1)
+fern_img <- get_phylopic(uuid = fern)
 
 ###### Mosses ######
  
-moss<- name_search(text = "Bryozoa", options = "namebankID")[[1]]
-moss
-
-moss_id <- name_images(uuid = moss$uid[1])
-
-moss_pic <- image_data("191ad6ce-78f2-444d-b4ad-9c4162b1803f", size = 256)[[1]]
+moss <- get_uuid(name = "Takakia lepidozioides", n = 1)
+moss_img <- get_phylopic(uuid = moss)
 
 ##### Crustacean #####
  
-crust<- name_search(text = "Decapoda", options = "namebankID")[[1]]
-crust
-
-crust_id <- name_images(uuid = crust$uid[1])
-
-crust_pic <- image_data("786bbbd0-529d-4e64-a8ac-f54f88194033", size = 256)[[1]]
-
+crust <- get_uuid(name = "Decapoda", n = 1)
+crust_img <- get_phylopic(uuid = crust)
 
 ####### "Arachnids"  ######
  
-arach<- name_search(text = "Araneidae", options = "namebankID")[[1]]
-arach
-
-arach_id <- name_images(uuid = arach$uid[1])
-
-arach_pic <- image_data("593cd880-1440-4562-b589-264cc6f9e5f2", size = 256)[[1]]
+arach <- get_uuid(name = "Argiope bruennichi", n = 1)
+arach_img <- get_phylopic(uuid = arach)
 
 ####### "Conifers and Cycads" #####
 
-conifer<- name_search(text = "Pinaceae", options = "namebankID")[[1]]
-conifer
-
-conifer_id <- name_images(uuid = conifer$uid[1])
-
-conifer_pic <- image_data("38c636a7-f4b3-4a6c-89b1-fb2dcbdafa06", size = 256)[[1]]
-
+conifer <- get_uuid(name = "Pinaceae", n = 1)
+conifer_img <- get_phylopic(uuid = conifer)
 
 #####  IMAGE #####
 
 USA_phylo <- usa_pic + 
   coord_flip() +
-  add_phylopic(bear_pic, alpha = 1, x = 8, y = 140, ysize = 20) +
-  add_phylopic(toad_pic, alpha = 1, x = 4, y = 140, ysize = 15) +
-  add_phylopic(fish_pic, alpha = 1, x = 5, y = 140, ysize = 25) +
-  add_phylopic(bird_pic, alpha = 1, x = 6.1, y = 140, ysize = 20) +
-  add_phylopic(reptile_pic, alpha = 1, x = 7, y = 140, ysize = 15) +
-  add_phylopic(flower_pic, alpha = 1, x = 3, y = 140, ysize = 20) +
-  add_phylopic(insect_pic, alpha = 1, x = 2, y = 140, ysize = 20) +
-  add_phylopic(clam_pic, alpha = 1, x = 1, y = 140, ysize = 20)
+  add_phylopic(bear_img, alpha = 1, x = 8, y = 1, ysize = 2) +
+  add_phylopic(toad_img, alpha = 1, x = 4, y = 1, ysize = 2) +
+  add_phylopic(fish_img, alpha = 1, x = 5, y = 1, ysize = 2) +
+  add_phylopic(bird_img, alpha = 1, x = 6.1, y = 1, ysize = 2) +
+  add_phylopic(reptile_img, alpha = 1, x = 7, y = 1, ysize = 2) +
+  add_phylopic(flower_img, alpha = 1, x = 3, y = 1, ysize = 2) +
+  add_phylopic(insect_img, alpha = 1, x = 2, y = 1, ysize = 2) +
+  add_phylopic(clam_img, alpha = 1, x = 1, y = 1, ysize = 2)
 
 
 can_phylo <- can_pic + 
   coord_flip() +
-  add_phylopic(bear_pic, alpha = 1, x = 9, y = 170, ysize = 20) +
-  add_phylopic(toad_pic, alpha = 1, x = 5, y = 140, ysize = 20) +
-  add_phylopic(fish_pic, alpha = 1, x = 7, y = 140, ysize = 25) +
-  add_phylopic(bird_pic, alpha = 1, x = 8, y = 140, ysize = 20) +
-  add_phylopic(reptile_pic, alpha = 1, x = 6, y = 140, ysize = 20) +
-  add_phylopic(flower_pic, alpha = 1, x = 1, y = 140, ysize = 25) +
-  add_phylopic(insect_pic, alpha = 1, x = 4, y = 140, ysize = 25) +
-  add_phylopic(clam_pic, alpha = 1, x = 2, y = 140, ysize = 20) +
-  add_phylopic(lichen_pic, alpha = 1, x = 3, y = 140, ysize = 20)
+  add_phylopic(bear_img, alpha = 1, x = 9, y = 1, ysize = 2) +
+  add_phylopic(toad_img, alpha = 1, x = 5, y = 1, ysize = 2) +
+  add_phylopic(fish_img, alpha = 1, x = 7, y = 1, ysize = 2) +
+  add_phylopic(bird_img, alpha = 1, x = 8, y = 1, ysize = 2) +
+  add_phylopic(reptile_img, alpha = 1, x = 6, y = 1, ysize = 2) +
+  add_phylopic(flower_img, alpha = 1, x = 1, y = 1, ysize = 2) +
+  add_phylopic(insect_img, alpha = 1, x = 4, y = 1, ysize = 2) +
+  add_phylopic(clam_img, alpha = 1, x = 2, y = 1, ysize = 2) +
+  add_phylopic(lichen_img, alpha = 1, x = 3, y = 1, ysize = 2)
 
 combine_phylopic <- USA_phylo + can_phylo
 
@@ -370,41 +299,41 @@ test +
   add_phylopic(lichen_pic, alpha = 1, x = 4, y = 10, ysize = 10) 
 
 
-##### double figure
+##### double figure #######
 
 usa_phylo <- usa_pic + 
-  add_phylopic(arach_pic, alpha = 1, x = 12, y = 0.1, ysize = 1) +
-  add_phylopic(conifer_pic, alpha = 1, x = 13, y = 0.1, ysize = 1) +
-  add_phylopic(lichen_pic, alpha = 1, x = 14, y = 0.1, ysize = 1) +
-  add_phylopic(crust_pic, alpha = 1, x = 11, y = 0.1, ysize = 1) +
-  add_phylopic(flower_pic, alpha = 1, x = 10, y = 0.15, ysize = 1) +
-  add_phylopic(clam_pic, alpha = 1, x = 9, y = 0.15, ysize = 1) +
-  add_phylopic(insect_pic, alpha = 1, x = 8, y = 0.15, ysize = 1) +
-  add_phylopic(snail_pic, alpha = 1, x = 7, y = 0.15, ysize = 1) +
-  add_phylopic(fish_pic, alpha = 1, x = 6, y = 0.15, ysize = 1) +
-  add_phylopic(fern_pic, alpha = 1, x = 5, y = 0.2, ysize = 1) +
-  add_phylopic(reptile_pic, alpha = 1, x = 4, y = 0.2, ysize = 1) +
-  add_phylopic(bear_pic, alpha = 1, x = 3, y = 0.3, ysize = 1) +
-  add_phylopic(bird_pic, alpha = 1, x = 2, y = 0.3, ysize = 1) +
-  add_phylopic(toad_pic, alpha = 1, x = 1, y = 0.4, ysize = 1) 
+  add_phylopic(arach_img, alpha = 1, x = 12, y = 0.1, ysize = 1) +
+  add_phylopic(conifer_img, alpha = 1, x = 13, y = 0.1, ysize = 1) +
+  add_phylopic(lichen_img, alpha = 1, x = 14, y = 0.1, ysize = 1) +
+  add_phylopic(crust_img, alpha = 1, x = 11, y = 0.1, ysize = 1) +
+  add_phylopic(flower_img, alpha = 1, x = 10, y = 0.15, ysize = 1) +
+  add_phylopic(clam_img, alpha = 1, x = 9, y = 0.12, ysize = 0.75) +
+  add_phylopic(insect_img, alpha = 1, x = 8, y = 0.15, ysize = 1) +
+  add_phylopic(snail_img, alpha = 1, x = 7, y = 0.15, ysize = 1) +
+  add_phylopic(fish_img, alpha = 1, x = 6, y = 0.15, ysize = 1) +
+  add_phylopic(fern_img, alpha = 1, x = 5, y = 0.2, ysize = 1) +
+  add_phylopic(reptile_img, alpha = 1, x = 4, y = 0.2, ysize = 1) +
+  add_phylopic(bear_img, alpha = 1, x = 3, y = 0.25, ysize = .75) +
+  add_phylopic(bird_img, alpha = 1, x = 2, y = 0.3, ysize = 1) +
+  add_phylopic(toad_img, alpha = 1, x = 1, y = 0.4, ysize = 1) 
   
  
   
 can_phylo <- can_pic + 
-  add_phylopic(bird_pic, alpha = 1, x = 1, y = 0.6, ysize = 0.75) +
-  add_phylopic(bear_pic, alpha = 1, x = 2, y = 0.45, ysize = 0.75) +
-  add_phylopic(toad_pic, alpha = 1, x = 3, y = 0.39, ysize = 0.8) +
-  add_phylopic(reptile_pic, alpha = 1, x = 4, y = 0.25, ysize = 1) +
-  add_phylopic(fish_pic, alpha = 1, x = 5, y = 0.25, ysize = 1) +
-  add_phylopic(lichen_pic, alpha = 1, x = 6, y = 0.17, ysize = 0.75) +
-  add_phylopic(insect_pic, alpha = 1, x = 7, y = 0.17, ysize = 1) +
-  add_phylopic(clam_pic, alpha = 1, x = 8, y = 0.15, ysize = 0.65) +
-  add_phylopic(flower_pic, alpha = 1, x = 9, y = 0.1, ysize = 0.75) +
-  add_phylopic(arach_pic, alpha = 1, x = 10, y = 0.1, ysize = 1) +
-  add_phylopic(conifer_pic, alpha = 1, x = 11, y = 0.13, ysize = 1.5) +
-  add_phylopic(fern_pic, alpha = 1, x = 12, y = 0.1, ysize = 0.7) +
-  add_phylopic(moss_pic, alpha = 1, x = 13, y = 0.1, ysize = 1) + 
-  add_phylopic(snail_pic, alpha = 1, x = 14, y = 0.1, ysize = 1)
+  add_phylopic(bird_img, alpha = 1, x = 1, y = 0.6, ysize = 0.75) +
+  add_phylopic(bear_img, alpha = 1, x = 2, y = 0.45, ysize = 0.75) +
+  add_phylopic(toad_img, alpha = 1, x = 3, y = 0.39, ysize = 0.8) +
+  add_phylopic(reptile_img, alpha = 1, x = 4, y = 0.25, ysize = 1) +
+  add_phylopic(fish_img, alpha = 1, x = 5, y = 0.25, ysize = 1) +
+  add_phylopic(lichen_img, alpha = 1, x = 6, y = 0.17, ysize = 0.75) +
+  add_phylopic(insect_img, alpha = 1, x = 7, y = 0.15, ysize = 0.75) +
+  add_phylopic(clam_img, alpha = 1, x = 8, y = 0.1, ysize = 0.65) +
+  add_phylopic(flower_img, alpha = 1, x = 9, y = 0.1, ysize = 0.75) +
+  add_phylopic(arach_img, alpha = 1, x = 10, y = 0.1, ysize = 1) +
+  add_phylopic(conifer_img, alpha = 1, x = 11, y = 0.13, ysize = 1.5) +
+  add_phylopic(fern_img, alpha = 1, x = 12, y = 0.1, ysize = 0.7) +
+  add_phylopic(moss_img, alpha = 1, x = 13, y = 0.1, ysize = 1.5) + 
+  add_phylopic(snail_img, alpha = 1, x = 14, y = 0.1, ysize = 1)
   
   
 combine_phylo <- usa_phylo + plot_spacer() + can_phylo +
@@ -415,9 +344,7 @@ combine_phylo
 
 
 ggsave("figures/combined_figure_phylo.TIFF",
-       combine_phylo,
-       height = 8.62,
-       width = 17.9)
+       combine_phylo)
    
 
   
